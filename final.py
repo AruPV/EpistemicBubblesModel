ed2f003b102d1cb150f280a471aac4d77ed637ad
alexis5679 <129790982+alexis5679@users.noreply.github.com>
2023-04-05 12:38:21 -0400

    Update final.py

--- final.py
+++ final.py
@@ -8,16 +8,29 @@ class Agent:
     '''
     def __init__(self, ID, location):
         self._ID = ID         # probably can just be a number
-        self._location = location   # row col position of the cell the agent is in
+        self._position = location   # row col position of the cell the agent is in
         self._information = None
 
         # things we can change later -- should add rates later
         self._new_info_rate = None  # the chance each agent has of generating
                                         # new information
-        self._spread_radius = None  # the radius outside the cell information
+        self._spread_radius = 2  # the radius outside the cell information
                                         # can spread
         self._acceptance_rate = None # how likely an agent is to accept new
                                         # information
+
+    def __eq__(self, other: 'Agent') -> bool:
+        ''' Boolean method to indicate whether a given Agent is equal to this agent
+        Returns:
+            True if this agent and the other agent are the same, False o/w
+        '''
+        return self._position.row == other._position.row and \
+               self._position.col == other._position.col and \
+               self._ID == other._ID
+
+    def __str__(self) -> None:
+        ''' creates str version of agent object '''
+        return f"Agent {str(self._ID)}, Location: ({self._position.row}, {self._position.col})"
 
 class Information:
     ''' allows us to use information as a class
@@ -65,7 +78,7 @@ class Cell:
 class Grid:
     ''' class representing a 2-D list of cell objects'''
 
-    slots = ('_num_rows', '_num_cols')
+    slots = ('_num_rows', '_num_cols', '_agents')
 
     def __init__(self, rows: int = 10, cols: int = 10, prop_agents: float = 0.3):
         ''' initializer method for grid object
@@ -74,10 +87,10 @@ class Grid:
             cols:           number of columns in the grid
             prop_agents:    proportion of cells that will contain an agent when
                             the grid is created
-
         '''
         self._num_rows = rows
         self._num_cols = cols
+        self._agents = {}
 
         # create a rows x cols 2D list of Cell objects
         self._grid: list[list[Cell]] = \
@@ -85,26 +98,125 @@ class Grid:
 
         g_copy = [cell for row in self._grid for cell in row]
         agents = random.sample(g_copy, k = round((rows * cols - 2) * prop_agents))
-        id_names = 0    # can't think of a better way to do this in my brain state
-                        # want to give each agent a name
+
         for i in agents:
             #create agent with an id and store cell location inside of it
             # append that agent object to cell contents
-            new_agent = Agent(id_names, i._position)
+            name = len(self._agents) + 1
+            new_agent = Agent(name, i._position)
             i._contents.append(new_agent)
-            id_names += 1
+            self._agents[name] = new_agent
+            print(i)
+        printDict(self._agents)
 
     def __str__(self) -> None:
         ''' creates a str version of the grid that shows each cell and the
         (number? names) of agents inside '''
         maze_str = ""
-        for row in self._grid:  # row : List[Cell]
+        for row in self._grid:
             maze_str += "|" + "|".join([str(len(cell._contents)) for cell in row]) + "|\n"
         return maze_str[:-1]  # remove the final \n
 
+    def _moveAgent(self):
+        pass
+
+    def _agentsInRange(self, origin: Agent) -> List:
+        ''' method to return a list of all the agents in within the spread radius
+        of the the inputted agent
+        Parameters:
+            origin: the agent that is spreading new information
+        Returns:
+            a list of all the agents within the spread radius
+        '''
+        # Im going to work under the assumption that a spread radius of 0
+            # means that the agent can only spread information to agents in the same cell
+            # and a spread of 1 means cells 1 cell away (including diagonals)
+            # can change this later
+        agents = []
+        if origin._spread_radius == 0:
+            for a in self._grid[origin._position.row][origin._position.col]._contents:
+                if a != origin:
+                    agents.append(a)
+        else:
+            # row_start
+            if origin._position.row - origin._spread_radius < 0:
+                row_start = 0
+            else: row_start = origin._position.row - origin._spread_radius
+            # row_end
+            if origin._position.row + origin._spread_radius > self._num_rows:
+                row_end = self._num_rows
+            else: row_end = origin._position.row + origin._spread_radius + 1
+            # col_start
+            if origin._position.col - origin._spread_radius < 0:
+                col_start = 0
+            else: col_start = origin._position.col - origin._spread_radius
+            # col_end
+            if origin._position.col + origin._spread_radius > self._num_cols:
+                col_end = self._num_cols
+            else: col_end = origin._position.col + origin._spread_radius + 1
+
+            # loop through all the cells and append all their agents to the list
+            for r in range(row_start, row_end):
+                for c in range(col_start, col_end):
+                    for a in self._grid[r][c]._contents:
+                        if a == origin:     # don't add origin agent to the list
+                            continue
+                        agents.append(a)
+        return agents
+
+    def _addAgent(self, position: Position) -> None:
+        # do we need this one? not sure
+        # also if we we're going to do this, might want to change it so that the
+            # agent object itself can be directly inputted -- might be a challenge
+            # with the way I have naming set up right now tho
+        ''' method to add an agent to a given cell in the grid -- NOTE: different
+        from move agent
+        Parameters:
+            the position on the grid where the agents should be added
+        Returns:
+            None
+        '''
+        new_agent = Agent(len(self._agents) + 1, position)
+        self._agents[len(self._agents) + 1] = new_agent
+        self._grid[position.row][position.col]._contents.append(new_agent)
+
+    def _findAgent(self, ID):
+        ''' method to find agent in object in grid using ID number
+        '''
+        return self._agents[ID]
+
+#### this is just printing the dictionary bc it is just not working for me
+def printDict(my_dictionary) -> str:
+    string = ""
+    for key in my_dictionary:
+
+        print(f"{key}: {my_dictionary[key]}")
+
 def main():
+    random.seed(42)
     g = Grid()
     print(g)
+
+    ################# testing addAgent
+    p = Position(1,1)
+    g._addAgent(p)
+    print(len(g._agents))
+    print(g)
+
+    #agent30 = g._findAgent(30)
+    #print(g._agentsInRange(agent30)[0])
+    ############## testing agentInRange when spread radius is 1
+    agent9 = g._findAgent(9)
+    p2 = Position(1,3)
+    g._addAgent(p2)
+    #test = g._agentsInRange(agent9)
+    #for a in test:
+        #print(a)
+
+    ############ testing agentInRange when spread radius is 2
+    test = g._agentsInRange(agent9)
+    for a in test:
+        print(a)
 
 
 
